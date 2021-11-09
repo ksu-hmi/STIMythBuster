c2ea97e3b204d576181c623d7353179b02160e5d
khigh6 <77521597+khigh6@users.noreply.github.com>
2021-11-08 19:44:07 -0500

    Update APP Development.py

--- APP Development.py
+++ APP Development.py
@@ -2,37 +2,36 @@
 #import the library
 import tkinter as Tk
 from tkinter import *
-from tkinter import filedialog, Text
+
 #create a window object
 root = Tk()
 
-
-#Define a function
-def addApp():
-    filename= filedialog.askopenfilename(initialdir="/", title="Select File",)
-    filetypes=(()))
-
-
-#setup the window size
-frame = Tk.Frame(root, width=600, height=600)
-#put the sizing  into our window (pack is the simplest method)
-frame.pack()
-
 #setup canvas size and color
-canvas = Canvas(root, width=600, height=900, bg="blue")
+canvas = Canvas(root, width=700, height=6700, bg="blue")
 canvas.pack()
 
-#Add a button to root
-openFile = Button(root, text="Open File", padx=10, pady=5, fg="white", 
-                            bg="red" command=addAPP) 
-openFile.pack()
+#setup the window size
+frame = Frame(root, bg="white")
+#put the sizing  into our window (pack is the simplest method)
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1
 
-runApps = Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="red") 
+
+#Create a label
+label = Label(frame, text='Welcome to STIMythBusters')
+label.pack()
+
+#Add a button to frame
+Button = Button(frame, text="SearchbySTI")
+Button.pack()
+
+#Add a button to root
+runApps = Button(root, text="Click Here", padx=10, pady=5, fg="white", bg="red") 
 runApps.pack()
 
 
 #title the overall window
 root.title("STIMythBuster")
+
 #show the window - windows run in a loop listening for the window close button to break the loop
 root.mainloop()
 
