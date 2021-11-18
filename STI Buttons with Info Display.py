1092b0faac603166995e1efbf55cbe272204c6a8
OanaT1 <89046912+OanaT1@users.noreply.github.com>
2021-11-18 15:00:57 -0500

    Changed font type to a more professional font option and button background colors to create color contrast.

--- STI Buttons with Info Display.py
+++ STI Buttons with Info Display.py
@@ -170,7 +170,7 @@ def click_setup():
     button1 = tk.Button(text='Chlamydia')
     button1.config(command=click) # performs call back of function
     button1.config(height = 5, width = 25)
-    button1.config(font=('Georgia', 15, 'bold'))
+    button1.config(font=('Comic Sans', 15, 'bold'))
     button1.config(bg='orange')
     button1.config(fg='white')
     button1.grid(row=0, column=0)
@@ -180,7 +180,7 @@ def click1_setup():
     button2 = tk.Button(text='Gonorrhea')
     button2.config(command=click1) # performs call back of function
     button2.config(height = 5, width = 25)
-    button2.config(font=('Georgia', 15, 'bold'))
+    button2.config(font=('Comic Sans', 15, 'bold'))
     button2.config(bg='#DE1F27')
     button2.config(fg='white')
     button2.grid(row=1, column=0)
@@ -189,8 +189,8 @@ def click2_setup():
     button3 = tk.Button(text='Human Papillomavirus')
     button3.config(command=click2) # performs call back of function
     button3.config(height = 5, width = 25)
-    button3.config(font=('Georgia', 15, 'bold'))
-    button3.config(bg='#B6DE1F')
+    button3.config(font=('Comic Sans', 15, 'bold'))
+    button3.config(bg='#1FDED3')
     button3.config(fg='white')
     button3.grid(row=2, column=0)
 
@@ -198,8 +198,8 @@ def click3_setup():
     button4 = tk.Button(text='Syphilis')
     button4.config(command=click3) # performs call back of function
     button4.config(height = 5, width = 25)
-    button4.config(font=('Georgia', 15, 'bold'))
-    button4.config(bg='#D4DE1F')
+    button4.config(font=('Comic Sans', 15, 'bold'))
+    button4.config(bg='#B6DE1F')
     button4.config(fg='white')
     button4.grid(row=3, column=0)
 
@@ -209,7 +209,7 @@ def click4_setup():
     button5 = tk.Button(text='Trichomoniasis')
     button5.config(command=click4) # performs call back of function
     button5.config(height = 5, width = 25)
-    button5.config(font=('Georgia', 15, 'bold'))
+    button5.config(font=('Comic Sans', 15, 'bold'))
     button5.config(bg='#1FDED6')
     button5.config(fg='white')
     button5.grid(row=4, column=0)
@@ -219,7 +219,7 @@ def click5_setup():
     button5 = tk.Button(text='Genital Herpes')
     button5.config(command=click4) # performs call back of function
     button5.config(height = 5, width = 25)
-    button5.config(font=('Georgia', 15, 'bold'))
+    button5.config(font=('Comic Sans', 15, 'bold'))
     button5.config(bg='#DE1FBC')
     button5.config(fg='white')
     button5.grid(row=4, column=0)
