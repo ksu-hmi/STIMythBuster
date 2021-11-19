91e85b05c78be28e5d6a2b696659ab03a2ae201a
khigh6 <77521597+khigh6@users.noreply.github.com>
2021-11-19 14:22:12 -0500

    Update STIMB Landing Page.py
    Updated code to include root title and a new canvas to include a button to learn more. This step is still in progress as it should serve as a new page with content. Currently the additional pages are divided into separate code. Ultimately, we would like to place code on new canvases all on one code. Working progress.

--- STIMB Landing Page.py
+++ STIMB Landing Page.py
@@ -1,12 +1,21 @@
 #import libraries
 import tkinter as tk
+from tkinter import *
 from PIL import ImageTk, Image
+
 
 #beginning of code
 root = tk.Tk()
+root.title("STIMythBusters Interactive Application")
 
 canvas = tk.Canvas(root, bg="purple", width=600, height=400)
 canvas.grid(columnspan=4, rowspan=4)
+
+canvas2 = tk.Canvas2(root, bg="purple", width=600, height=400)
+canvas2.grid(columnspan=4, rowspan=4)
+
+frame = tk.Canvas2(root, bg="blue", width=600, height=400)
+frame.grid(x=20, y=20)
 
 #Open logo
 load = Image.open("logo.png")
@@ -21,14 +30,22 @@ img.place(x=20, y=20)
 instructions = tk.Label(root, text="Welcome to STIMythBusters", font="Raleway", bg="brown", fg="white")
 instructions.grid(columnspan=4, column=0, row=0)
 
+
 def open_file():
     browser_text.set("loading....")
     canvas2 = tk.Canvas(root, bg="purple", width=600, height=400)
     canvas2.grid(columnspan=4, rowspan=4) 
 
-details = tk.Canvas(root, text="Search STI Symptoms to better understand the Sexually Transmitted Infection", font="Raleway", bg="brown", fg="white")
+#button = tk.Button(canvas2, text="new window",command=lambda:open_file(), bg='black', fg='#469A00', )
+#button.grid(column=1, row=2)
     
+#create button that will be placed on canvas2
+browser_text2 = tk.StringVar()
+browse_btn= tk.Button(canvas2, textvariable=browser_text2, command=lambda:open_file(), font="Raleway", bg="green", fg="white", height=2, width=15)
+browser_text2.set("LEARN MORE")
+browse_btn.grid(column=1, row=2)
     
+
 #browser button 1
 browser_text = tk.StringVar()
 browse_btn= tk.Button(root, textvariable=browser_text, command=lambda:open_file(), font="Raleway", bg="green", fg="white", height=2, width=15)
@@ -37,6 +54,8 @@ browse_btn.grid(column=1, row=2)
 
 def open_button():
     browse_text.set("loading....")
+    canvas2 = tk.Canvas(root, bg="purple", width=600, height=400)
+    canvas2.grid(columnspan=4, rowspan=4) 
 
 #browse button 2
 browse_text = tk.StringVar()
