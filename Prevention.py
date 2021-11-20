53dfdf2a6ecb6cf79950d9d70a1f0e6ae9a798e9
arnettgl <garnett1@students.kennesaw.edu>
2021-11-20 05:48:16 -0500

    Update Prevention.py
    Changed background color,  size of button,  and font configuration.

--- Prevention.py
+++ Prevention.py
@@ -7,8 +7,15 @@ def open():
 
 root = Tk()
 
-res_btn = Button(root, text= "Prevention",command=open, padx=100, pady=40, fg="Fuchsia", bg="dark blue")
+res_btn = Button(root, text= "Prevention",command=open, padx=100, pady=40, fg="Fuchsia", bg="Light Blue")
 
 res_btn.pack()
+
+def resize(e):
+    size = e.width / 20
+    res_btn.config(font=("Helveltica", int(size)))
+  
+
+root.bind('<Configure>', resize)
 
 root.mainloop()
