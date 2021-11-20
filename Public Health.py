b57f5cf086d58e4e697b4dca6b9ad290cd5e7e13
arnettgl <garnett1@students.kennesaw.edu>
2021-11-20 04:36:39 -0500

    Update Public Health.py
    Updated colors of font and background of public health button

--- Public Health.py
+++ Public Health.py
@@ -7,7 +7,7 @@ def open():
 
 root = Tk()
 
-res_btn = Button(root, text= "GA Dept of Public Health",command=open, padx=300, pady=300, fg="fuchsia", bg="dark blue")
+res_btn = Button(root, text= "GA Dept of Public Health",command=open, padx=300, pady=300, fg="yellow", bg="teal")
 
 res_btn.pack()
 
