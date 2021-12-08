8bf832ed3f264fde41b92ead4efacf796db8bc69
khigh6 <77521597+khigh6@users.noreply.github.com>
2021-12-07 19:27:15 -0500

    Updated

--- Public Health.py
+++ Public Health.py
@@ -7,7 +7,7 @@ def open():
 
 root = Tk()
 
-res_btn = Button(root, text= "GA Dept of Public Health",command=open, padx=30, fg="fuchsia", bg="dark blue")
+res_btn = Button(root, text= "GA Dept of Public Health",command=open, padx=300, pady=300, fg="fuchsia", bg="dark blue")
 
 res_btn.pack()
 
